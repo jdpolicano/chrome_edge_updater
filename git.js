78b3a96e15b4477292b3809dcd95fe1411ddd37e
= <=>
2024-12-08 19:30:23 -0500

    chore: update edge version

--- git.js
+++ git.js
@@ -37,6 +37,6 @@ export function addCommitPush(chromeChanged, edgeChanged, dryRun = false) {
 function printGitPlan(commitMsg) {
   log("dryRun flag present, would run the following commands:");
   log("- git add .");
-  log(`- git commit -m '${commitMsg}'`);
+  log(`- git commit -m '[skip-ci] ${commitMsg}'`);
   log("- git push");
 }
